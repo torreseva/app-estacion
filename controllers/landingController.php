3b4f1d8dc85ce60dd3669631a452d40db5b6d4f2
Matias Leonardo Baez <elmattprofe@gmail.com>
2023-07-11 19:10:07 -0300

    mamut-1002-Se agrego el control de existencia de variables dentro de las plantillas.

--- controllers/landingController.php
+++ controllers/landingController.php
@@ -1,9 +1,11 @@
 <?php 
 
-	$tpl = new Primel('views/landingView.html');
+	$tpl = new Primel('views/langView.html');
 
 	$tpl->assign("usuarios", 6666);
 	$tpl->assign("ACTUAL_YEAR",date('Y'));
+
+	$tpl->assign("duende", 6666);
 
 	$tpl->printToScreen();
  ?>
--- lib/primel.php
+++ lib/primel.php
@@ -7,14 +7,27 @@
 	{
 
 		public $tpl = '';
+		public $url_tpl = '';
 		
 		function __construct($url_tpl)
 		{
+			$this->url_tpl = $url_tpl;
 			$this->tpl = file_get_contents($url_tpl);
 		}
 
+		/**
+		 * 
+		 * pasa un valor a una variable de la plantilla
+		 * 
+		 * */
 		public function assign($needle, $value){
-			$this->tpl = str_replace("{{{$needle}}}", $value, $this->tpl);
+
+			if(strstr($this->tpl, "{{{$needle}}}")){
+				$this->tpl = str_replace("{{{$needle}}}", $value, $this->tpl);
+			}else{
+				echo "<b>Error en plantilla ({$this->url_tpl}):</b> No existe la variable <u>$needle</u>";
+				exit();
+			}
 		}
 
 		public function printToScreen(){
